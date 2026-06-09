cdad971aedce4a1e5ce1e5337be995eb0a9957f3
alina0607 <huiyu06078@gmail.com>
2026-06-08 22:45:49 -0700

    Add UE5 project

---
+++ Game/ConnectFourRL/Source/ConnectFourRL/ConnectFourRL.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ConnectFourRL.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ConnectFourRL, "ConnectFourRL" );
